98784f745dad0e3768e4160f25482317bff164bf
adamgado <adamgado33@gmail.com>
2024-05-07 10:13:07 -0700

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""queries the Reddit API and returns the number of subscribers"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    """returns the number of subscribers for a given subreddit"""
+    link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/edev_)"
+    }
+    r = requests.get(link, headers=headers, allow_redirects=False)
+    if r.status_code == 404:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
+    a = r.json().get("data")
+    return a.get("subscribers")
