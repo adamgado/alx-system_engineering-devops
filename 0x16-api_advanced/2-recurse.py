1488a92f619ec44ea08f4c604594d5eaa1fa939f
adamgado <adamgado33@gmail.com>
2024-08-13 20:51:30 +0000

    2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,16 +3,17 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", num=0):
+def recurse(subreddit, hot_list=[], count=0, after=""):
     """returns a list containing the titles of all hot articles"""
     lnk = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     req = requests.get(lnk, headers={"User-Agent": "simplemitten"}
-                       , params={"after": after, "count": num, "limit": 10} allow_redirects=False)
+                       , params={"after": after, "count": count, "limit": 10}
+                       , allow_redirects=False)
     if req.status_code == 404:
         return None
     result = req.json().get("data")
     after = results.get("after")
-    num += results.get("dist")
+    count += results.get("dist")
     for a in results.get("children"):
         hot_list.append(a.get("data").get("title"))
     if after is not None:
