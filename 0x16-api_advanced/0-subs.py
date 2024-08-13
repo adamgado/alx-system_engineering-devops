9c2ff8455d260b489a3bd7eeae25831b07bd95c7
adamgado <adamgado33@gmail.com>
2024-08-13 19:04:47 +0000

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
+"""queries the Reddit API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-  """Fetches the subscriber count of a given subreddit using the Reddit API.
-
-  Args:
-    subreddit_name: The name of the subreddit.
-
-  Returns:
-    The number of subscribers for the subreddit, or None if an error occurs.
-  """
-
-  url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-  response = requests.get(url)
-
-  if response.status_code == 200:
-    data = response.json()
-    return data['data']['subscribers']
-  else:
-    return 0
+    """returns the number of subscribers"""
+    lnk = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    req = requests.get(lnk, headers={"User-Agent": "simplemitten"}, allow_redirects=False)
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
+        return 0
