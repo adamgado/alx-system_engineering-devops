25f6a44fccc82f40d07a217a34fe1e4ced70af5b
adamgado <adamgado33@gmail.com>
2024-08-13 20:10:58 +0000

    1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""queries the Reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts listed"""
+    lnk = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    req = requests.get(lnk, headers={"User-Agent": "simplemitten"}, params={"limit": 10}, allow_redirects=False)
+    if req.status_code == 200:
+        result = req.json().get("data")
+        for a in results.get("children"):
+            print(a.get("data").get("title"))
+    else:
+        print("None")
