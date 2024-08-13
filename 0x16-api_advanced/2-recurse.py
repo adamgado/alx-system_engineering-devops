26d3cc3fd169f6344e7bc3b16a87112bb835ee2e
adamgado <adamgado33@gmail.com>
2024-08-13 20:39:58 +0000

    2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""queries the Reddit API"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], next="", num=0):
+    """returns a list containing the titles of all hot articles"""
+    lnk = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    req = requests.get(lnk, headers={"User-Agent": "simplemitten"}
+                       , params={"after": next, "count": num, "limit": 10}, allow_redirects=False)
+    if req.status_code == 404:
+        return None
+    result = req.json().get("data")
+    next = results.get("after")
+    num += results.get("dist")
+    for a in results.get("children"):
+        hot_list.append(a.get("data").get("title"))
+    if after is not None:
+        return recurse(subreddit, hot_list, next, num)
+    return hot_list
