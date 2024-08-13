ea81eca48bb46876fb044fcc984e94bf53d82223
adamgado <adamgado33@gmail.com>
2024-08-13 20:54:18 +0000

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     lnk = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     req = requests.get(lnk, headers={"User-Agent": "simplemitten"}, allow_redirects=False)
-    if req.status_code < 300:
+    if req.status_code == 200:
         return req.json().get("data").get("subscribers")
     else:
         return 0
