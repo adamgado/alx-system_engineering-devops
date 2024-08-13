d4884be7b95cdbe0c0e411d076f60015627ae692
adamgado <adamgado33@gmail.com>
2024-08-13 21:15:41 +0000

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ import requests
 
 def top_ten(subreddit):
     """prints the titles of the first 10 hot posts listed"""
-    lnk = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    lnk = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     req = requests.get(lnk, headers={"User-Agent": "simplemitten"},
                        params={"limit": 10}, allow_redirects=False)
     if req.status_code == 200:
